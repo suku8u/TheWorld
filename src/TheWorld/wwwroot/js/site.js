19a8d5ff75d14ba482f225db3c31ddc90c380dfb
Sukumar Ponram <suku8u@uw.edu>
2016-10-05 21:21:39 -0700

    Add project files.

---
+++ src/TheWorld/wwwroot/js/site.js
@@ -0,0 +1,28 @@
+﻿
+(function () {
+    //var ele = $("#username");
+    //ele.text("Sukumar Ponram");
+
+
+    //var main = $("#main");
+    //main.on ("mouseenter", function () {
+    //    main.style = "background-color : #888;";
+    //});
+
+    //main.on ("mouseleave", function () {
+    //    main.style.backgroundColor = "";
+    //});
+
+    //var menuItems = $("ul.menu li a");
+    //menuItems.on("click", function () {
+    //    var me = $(this);
+    //    alert(me.text());
+    //})
+
+    var $sidebarAndWrapper = $("#sidebar, #wrapper");
+
+    $("#sidebarToggle").on("click", function () {
+        $sidebarAndWrapper.toggleClass("hide-sidebar");
+    })
+
+})();
